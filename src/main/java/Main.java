3fac7ae744c2f97609383b6bc49b18b19364e56d
Nacim <71361499+Nacim21@users.noreply.github.com>
2023-07-13 12:01:28 -0600

    Added tracklist.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import Player.Track;
+import UI.TrackList;
 import com.mpatric.mp3agic.InvalidDataException;
 import com.mpatric.mp3agic.UnsupportedTagException;
 import java.io.IOException;
@@ -9,12 +10,22 @@ public class Main {
     public static void main(String[] args) throws InvalidDataException, UnsupportedTagException, IOException {
         Scanner scnr = new Scanner(System.in);
 
-        System.out.println("Please enter a mp3 file path to play");
-        String filePath = scnr.nextLine();
+//        System.out.println("Please enter a mp3 file path to play");
+//        String filePath = scnr.nextLine();
+//
+//        Track track = new Track(filePath);
+//
+//        System.out.println(track.toString());
 
-        Track track = new Track(filePath);
 
-        System.out.println(track.toString());
+        TrackList trackList = new TrackList();
+
+        System.out.println("Please enter a filepath");
+        String bruh = scnr.nextLine();
+
+        System.out.println(trackList.fetchFiles(bruh));
+
+
 
 
 
--- src/main/java/UI/TrackList.java
+++ src/main/java/UI/TrackList.java
@@ -1,4 +1,36 @@
 package UI;
 
+import Player.Track;
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.UnsupportedTagException;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+
 public class TrackList {
+
+    public ArrayList<Track> fetchFiles(String directoryPath){
+        Path path = Paths.get(directoryPath);
+
+        ArrayList<Track> compatibleTracks= new ArrayList<Track>();
+
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+            for (Path entry : stream) {
+                if(entry.toString().endsWith(".mp3")){
+                    Track track = new Track(entry.toString());
+                    compatibleTracks.add(track);
+                }
+
+            }
+        } catch (IOException | InvalidDataException | UnsupportedTagException e) {
+            e.printStackTrace();
+        }
+
+        return compatibleTracks;
+    }
 }
